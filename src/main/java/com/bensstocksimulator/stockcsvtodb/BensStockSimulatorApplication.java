df1c7049a62b1c8804dc5d6e6b77a94a5d52795c
Ben Clark <beneclark@gmail.com>
2021-05-31 14:12:14 -0500

    Update application name

--- src/main/java/com/bensstocksimulator/stockcsvtodb/BensStockSimulatorApplication.java
+++ src/main/java/com/bensstocksimulator/stockcsvtodb/BensStockSimulatorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StockCsvToDbApplication {
+public class BensStockSimulatorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StockCsvToDbApplication.class, args);
+        SpringApplication.run(BensStockSimulatorApplication.class, args);
     }
 
 }
--- src/test/java/com/bensstocksimulator/stockcsvtodb/BensStockSimulatorApplicationTests.java
+++ src/test/java/com/bensstocksimulator/stockcsvtodb/BensStockSimulatorApplicationTests.java
@@ -2,12 +2,14 @@ package com.bensstocksimulator.stockcsvtodb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
 
 @SpringBootTest
-class StockCsvToDbApplicationTests {
+class BensStockSimulatorApplicationTests {
 
     @Test
-    void contextLoads() {
+    void validStockCSVUpload() {
+
 
     }
 
